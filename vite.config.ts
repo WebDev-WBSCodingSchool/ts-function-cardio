c67191ede9f727536078749c4e6c39b580231870
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2025-06-26 13:57:33 +0200

    add vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
+import checker from "vite-plugin-checker"
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
-  plugins: [tailwindcss()]
+  plugins: [tailwindcss(), checker({typescript: true})]
 });
