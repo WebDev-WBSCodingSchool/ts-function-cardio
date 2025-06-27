b52b548c44244e259647af47b4ce52665b4fb3a8
Jorge Paul <jserranopaul@gmail.com>
2025-06-27 07:44:57 +0200

    Vite Plugin Checker initial state and styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
-import checker from "vite-plugin-checker"
 import tailwindcss from '@tailwindcss/vite';
+import checker from 'vite-plugin-checker';
+
 export default defineConfig({
-  plugins: [tailwindcss(), checker({typescript: true})]
+  plugins: [
+    tailwindcss(),
+    checker({
+      typescript: true,
+      overlay: {
+        initialIsOpen: false,
+        position: 'br'
+      }
+    })
+  ]
 });
